ec62f521856d6be8dc965fca8c53eae8c33dcd0c
Kosjakov Evgeniy <42_@mail.ru>
2015-07-02 14:52:46 +0300

    First commit
    Solution added

---
+++ ScreenshotTaker/Taker.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ScreenshotTaker
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public static class Taker
+    {
+        private static void SaveScreen(this Image img, string path, string name, ImageFormat format)
+        {
+            var fullPath = path + @"\" + name + "." + format;
+            if (!Directory.Exists(path.TrimEnd('\\')))
+            {
+                Directory.CreateDirectory(path.TrimEnd('\\'));
+            }
+            Console.WriteLine("Saving screenshot: " + fullPath);
+            img.Save(fullPath, format);
+            Console.WriteLine("Saved.");
+        }
+
+        /// <summary>
+        /// Method to get currently executing assembly path
+        /// </summary>
+        /// <returns>Path to the assembly</returns>
+        public static string GetPath()
+        {
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var path = Path.GetDirectoryName(Uri.UnescapeDataString(uri.Path));
+            return path + @"\_Screenshots";
+        }
+
+        /// <summary>
+        /// Method to clean folder with screenshots
+        /// </summary>
+        public static void DeleteScreenshots()
+        {
+            var folder = new DirectoryInfo(GetPath());
+
+            foreach (var file in folder.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (var dir in folder.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+
+        /// <summary>
+        /// Method to take screenshot from primary screen and save it into the folder
+        /// </summary>
+        public static void TakeScreenshot()
+        {
+            var now = DateTime.Now;
+            var screenPath = GetPath();
+            var screenName = String.Format("screenshot_{0}",
+                now.ToString("yyyyMMddhhmmfff"));
+
+            using (var bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                            Screen.PrimaryScreen.Bounds.Height))
+            {
+                using (var g = Graphics.FromImage(bmpScreenCapture))
+                {
+                    g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                     Screen.PrimaryScreen.Bounds.Y,
+                                     0, 0,
+                                     bmpScreenCapture.Size,
+                                     CopyPixelOperation.SourceCopy);
+                    bmpScreenCapture.SaveScreen(screenPath, screenName, ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
